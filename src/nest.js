1ec5d5ae4ef8e7644c572cc5c7d9516a023e4e64
Liviu Tudor <liviu.tudor@gmail.com>
2017-03-06 22:20:05 -0800

    adding commander

--- src/nest.js
+++ src/nest.js
@@ -1,3 +1,18 @@
-const conf = require("../conf.json")
 const Nightmare = require("nightmare")
+const program = require("commander")
 
+let pathToConfig = "../conf.json"
+program
+	.option("-f <configfile>", "Specify a configuration file (use conf.json as default if not specified)")
+	.action(function(configfile) {
+		console.log( "Using configuration file " + configfile)
+		pathToConfig = configfile
+	})
+	.parse(process.argv)
+const conf = require(pathToConfig)
+console.log("conf.username=" + conf.username)
+
+// var nightmare = Nightmare()
+
+// nightmare.goto("https://home.nest.com/")
+// 		.wait()
