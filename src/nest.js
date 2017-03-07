9fd3d9942ab787eedb12018f2dea187c0afdd355
Liviu Tudor <liviu.tudor@gmail.com>
2017-03-06 22:59:52 -0800

    first cut need to figure out video

--- src/nest.js
+++ src/nest.js
@@ -1,18 +1,33 @@
-const Nightmare = require("nightmare")
-const program = require("commander")
+let Nightmare = require("nightmare")
+let program = require("commander")
 
 let pathToConfig = "../conf.json"
 program
-	.option("-f <configfile>", "Specify a configuration file (use conf.json as default if not specified)")
-	.action(function(configfile) {
-		console.log( "Using configuration file " + configfile)
-		pathToConfig = configfile
-	})
-	.parse(process.argv)
-const conf = require(pathToConfig)
-console.log("conf.username=" + conf.username)
+	.option( "-f, --file <configfile>", "Specify a configuration file (use conf.json as default if not specified)" )
+	.parse( process.argv )
 
-// var nightmare = Nightmare()
+if( program.file ) {
+	pathToConfig = program.file
+	console.log( "Using configuration file " + pathToConfig )
+}
+const conf = require( pathToConfig )
+console.log( "Using username " + conf.username )
 
-// nightmare.goto("https://home.nest.com/")
-// 		.wait()
+let nightmare = Nightmare({ show: true })
+nightmare
+	.useragent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:51.0) Gecko/20100101 Firefox/51.0")
+	.viewport(1024, 768)
+
+nightmare
+		.goto("https://home.nest.com/login")
+		.wait()
+		.type("#ember565", conf.username)
+		.type("#ember617", conf.password)
+		.click("#ember626")
+		.wait(10000)
+		.goto( "https://home.nest.com/camera/" + conf.cameraId)
+		.wait(10000)
+		.screenshot("1.png")
+		.end()
+		.then( () => console.log("Finished") )
+		.catch( (error) => console.log("ERROR: " + error) )
